38d549ddb15512fdc3d57dfafe2853f7945658ce
AbdElrahmanGbr <abdelrahman.gbr.os@gmail.com>
2023-04-16 23:11:36 +0200

    feat: ERD, README.md, fixed deps

--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: true,
-  appName: "Udagram",
-  apiHost: "http://localhost:8080/api/v0",
+  appName: 'Udagram-Udacity-Nanodegree',
+  apiHost: 'http://udagramapi-env-1.eba-wuibmf7u.us-east-1.elasticbeanstalk.com/api/v0',
 };
 
 /*
