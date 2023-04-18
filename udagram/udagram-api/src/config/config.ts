95282a471d4507858e3ac3ed8f7f336feb8bb53c
AbdElrahmanGbr <abdelrahman.gbr.os@gmail.com>
2023-04-18 19:30:05 +0200

    moved docs and screenshots to root dir, removed hardcoded env variables

--- udagram/udagram-api/src/config/config.ts
+++ udagram/udagram-api/src/config/config.ts
@@ -7,18 +7,18 @@ dotenv.config();
 // Are Also needed
 
 export const config = {
-  username: `gbr`,
-  password: "password123#",
-  database: "udagram",
-  host: "udagram.c1dvq4j92zcn.us-east-1.rds.amazonaws.com",
-  aws_region: "",
-  AWS_DEFAULT_REGION: "",
-  aws_profile: "",
-  aws_media_bucket: "",
-  url: "http://localhost:8080",
+  username: `${process.env.POSTGRES_USERNAME}`,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB,
+  host: process.env.POSTGRES_HOST,
+  aws_region: process.env.AWS_REGION,
+  AWS_DEFAULT_REGION: process.env.AWS_DEFAULT_REGION,
+  aws_profile: process.env.AWS_PROFILE,
+  aws_media_bucket: process.env.AWS_BUCKET,
+  url: process.env.URL,
   jwt: {
     secret: process.env.JWT_SECRET,
   },
-  accessKeyId : "",
-  secretAccessKey : ""
+  accessKeyId : process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey : process.env.AWS_SECRET_ACCESS_KEY
 };
