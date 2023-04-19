000669cc2e7dcf24533953f789e9394200ba691a
abdelrahman <abdelrahman.gbr.os@gmail.com>
2023-04-19 03:59:45 +0200

    refactoring deployment scripts

--- udagram/udagram-api/src/config/config.ts
+++ udagram/udagram-api/src/config/config.ts
@@ -12,13 +12,12 @@ export const config = {
   database: process.env.POSTGRES_DB,
   host: process.env.POSTGRES_HOST,
   aws_region: process.env.AWS_REGION,
-  AWS_DEFAULT_REGION: process.env.AWS_DEFAULT_REGION,
   aws_profile: process.env.AWS_PROFILE,
   aws_media_bucket: process.env.AWS_BUCKET,
   url: process.env.URL,
   jwt: {
     secret: process.env.JWT_SECRET,
   },
-  accessKeyId : process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey : process.env.AWS_SECRET_ACCESS_KEY
+  accessKeyId : "",
+  secretAccessKey : ""
 };
